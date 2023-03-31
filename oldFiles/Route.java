73b2a1241dc5fa7b1334096592127b7c91d28062
Muhammed Ümit Tavus <umit.tavus@ug.bilkent.edu.tr>
2023-04-01 00:14:52 +0300

    moving old files

---
+++ oldFiles/Route.java
@@ -0,0 +1,32 @@
+public class Route {
+    // Instance Variables
+    private String from;
+    private String to;
+
+    // Constructor
+    public Route(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    // Getters
+    public String getFrom() {
+        return this.from;
+    }
+    public String getTo() {
+        return this.to;
+    }
+
+    // Setters
+    public void setFrom(String from) {
+        this.from = from;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    // toString method
+    public String toString()
+    {
+        return this.from + "-->" + this.to;
+    }
+}
