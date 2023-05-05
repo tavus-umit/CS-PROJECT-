bbf39026ef053002bc4edd7ff4bfe37329abaf4d
EmirhanKılıç <emirhan.kilic@ug.bilkent.edu.tr>
2023-05-05 16:12:56 +0300

    adding elemnts to mainLoginPage

--- bilKayCs102/src/main/java/Bilkay/mainLoginMenu.java
+++ bilKayCs102/src/main/java/Bilkay/mainLoginMenu.java
@@ -6,9 +6,24 @@ public class mainLoginMenu {
 
 
     private JPanel mainPanelForMenu;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JPanel topLeftPanel;
+    private JPanel bottomLeftPanel;
+    private JLabel logoLabel;
+    private JLabel mottoLabel;
+    private JPanel rightCenter;
+    private JTextField usernameTextField;
+    private JButton loginButton;
+    private JButton button2;
+    private JButton forgotPasswordButton;
+    private JPasswordField passwordPasswordField;
 
     public JPanel getMainPanelForMenu() {
         return mainPanelForMenu;
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
