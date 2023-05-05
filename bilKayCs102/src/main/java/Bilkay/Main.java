273a24416d5020bb17eff9229b074ed5495fb524
EmirhanKılıç <emirhan.kilic@ug.bilkent.edu.tr>
2023-05-05 15:41:29 +0300

    adding proramManger and mainLoginMenu design

--- bilKayCs102/src/main/java/Bilkay/Main.java
+++
@@ -1,7 +0,0 @@
-package Bilkay;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ bilKayCs102/src/main/java/Bilkay/mainLoginMenu.java
@@ -0,0 +1,14 @@
+package Bilkay;
+
+import javax.swing.*;
+
+public class mainLoginMenu {
+
+
+    private JPanel mainPanelForMenu;
+
+    public JPanel getMainPanelForMenu() {
+        return mainPanelForMenu;
+    }
+
+}
