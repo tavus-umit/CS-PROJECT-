3c10744726b59b68cf7c6d8815516466fe9acca7
EmirhanKılıç <emirhan.kilic@ug.bilkent.edu.tr>
2023-05-07 17:19:20 +0300

    new icons and dashboard settings

--- bilKayCs102/src/main/java/Bilkay/mainDashboardMenu.java
+++ bilKayCs102/src/main/java/Bilkay/mainDashboardMenu.java
@@ -1,18 +1,35 @@
 package Bilkay;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class mainDashboardMenu {
 
     private final JFrame myMainFrame;
     private JPanel mainPanelForMenu;
-    private JPanel leftSideMenuPanel;
+    private JPanel leftSideMenuBackgroundPanel;
     private JPanel rightMainDashboardPanel;
-    private JButton button1;
     private JButton button2;
+    private JPanel leftSideMenuForegroundPanel;
+    private JButton buttonForThesubButton;
+    private JPanel topUserPanel;
+    private JPanel bottomMenuPanel;
+    private JLabel userPP;
+    private JLabel nameOfTheUserLabel;
+    private JLabel pointsOfTheUserLabel;
 
     public mainDashboardMenu(JFrame myMainFrameInput) {
         this.myMainFrame = myMainFrameInput;
+        leftSideMenuForegroundPanel.setMixingCutoutShape(new Rectangle2D.Double());
+
+
+        ImageIcon iconPP = new ImageIcon(new ImageIcon("C:\\Users\\emirh\\OneDrive\\Masaüstü\\CS-PROJECT-\\bilKayCs102\\src\\main\\resources\\test.jpeg").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+        userPP.setIcon(iconPP);
+
+        nameOfTheUserLabel.setText("Emirhan Kılıç");
+        pointsOfTheUserLabel.setText("46 pnts");
+
     }
 
     public JPanel getMainPanelForMenu() {
