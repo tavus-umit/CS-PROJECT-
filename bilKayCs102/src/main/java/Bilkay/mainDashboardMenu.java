eee981f708037d4725df78ba07b38d62fd7a96a4
EmirhanKılıç <emirhan.kilic@ug.bilkent.edu.tr>
2023-05-15 03:27:07 +0300

    .

--- bilKayCs102/src/main/java/Bilkay/mainDashboardMenu.java
+++ bilKayCs102/src/main/java/Bilkay/mainDashboardMenu.java
@@ -2,7 +2,8 @@ package Bilkay;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class mainDashboardMenu {
 
@@ -24,6 +25,14 @@ public class mainDashboardMenu {
     private JButton peopleButton;
     private JButton mailButton;
     private JButton bilUberButton;
+    private JPanel homeMenuPanel;
+    private JPanel profileMenuPanel;
+    private JPanel mathcesMenuPanel;
+    private JPanel eventsMenuPanel;
+    private JPanel biluberMenuPanel;
+    private JPanel emailMenuPanel;
+    private JPanel gymMenuPanel;
+    private JPanel settingsMenuPanel;
 
     private user currentUser;
 
@@ -40,6 +49,151 @@ public class mainDashboardMenu {
         pointsOfTheUserLabel.setText(String.valueOf(currentUser.getBilkayPoints()));
 
 
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(true);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(false);
+                webButton.setSelected(false);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(homeMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+
+            }
+        });
+        peopleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(false);
+                webButton.setSelected(false);
+                peopleButton.setSelected(true);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(mathcesMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
+        webButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(false);
+                webButton.setSelected(true);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(eventsMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
+        bilUberButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(false);
+                webButton.setSelected(false);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(true);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(biluberMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
+        mailButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(false);
+                webButton.setSelected(false);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(true);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(emailMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
+        gymButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(true);
+                webButton.setSelected(false);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(gymMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
+        settingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(true);
+                gymButton.setSelected(false);
+                webButton.setSelected(false);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(false);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(settingsMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
+        userPP.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homeButton.setSelected(false);
+                settingsButton.setSelected(false);
+                gymButton.setSelected(false);
+                webButton.setSelected(false);
+                peopleButton.setSelected(false);
+                mailButton.setSelected(false);
+                bilUberButton.setSelected(false);
+                userPP.setSelected(true);
+
+                rightMainDashboardPanel.removeAll();
+                rightMainDashboardPanel.add(profileMenuPanel);
+                myMainFrame.revalidate();
+                myMainFrame.repaint();
+            }
+        });
     }
 
     public JPanel getMainPanelForMenu() {
